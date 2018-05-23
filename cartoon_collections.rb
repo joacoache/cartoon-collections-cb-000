69f97fb79bac05fefad5bfcdd3c63cb5ff0c0b3d
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-05-23 22:35:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  hash = {}
+  roll_call_dwarves.each_with_index do |item, index|
+    hash[index] = item
+  end
+  puts hash  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
