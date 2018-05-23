a9502f6fe54297b80eba9bb7c12c473fc986e2db
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-05-23 23:33:42 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,5 +25,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find {|cheese| cheese.include?("chedadar" || "gouda" || "camembert")}
+  array.find {|cheese| cheese.include?("cheddar" || "gouda" || "camembert")}
 end
