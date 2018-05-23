01fa690bc8586f44e840da668db5758a04b19fd7
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-05-23 22:37:22 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,17 +1,22 @@
 def roll_call_dwarves(array)
   hash = {}
+
   roll_call_dwarves.each_with_index do |item, index|
     hash[index] = item
   end
-  puts hash  
+
+  hash.each_with_index do |item, index|
+    puts "#{index}. #{item}"
+  end  
+    
 end
 
 def summon_captain_planet(array)
-  
+
 end
 
 def long_planeteer_calls(array)
-  
+
 end
 
 def find_the_cheese(array)
